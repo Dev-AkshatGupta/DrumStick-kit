58d722408b348de60a59f8faf13f6cf9596c51b9
anuakshat <53713926+anuakshat@users.noreply.github.com>
2021-11-25 14:11:09 +0530

    styles: updated styles and Bug:fixed

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 window.addEventListener("keydown",e=>{
-    console.log(e.keyCode);
+    //console.log(e.keyCode);
     const audio=document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const keys=document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if(!audio)return
     audio.currentTime=0;
     audio.play();
     keys.classList.add(`playing`);
-        });
-        function removeTransition(e){
-            console.log(e);
-        }
-        const keys=document.querySelectorAll(".key");
-        keys.forEach(items=>{items.addEventListener("transitionend",removeTransition)});
+   });
+
+   const keys=document.querySelectorAll('.key');
+//    console.log(keys);
+   function removeTransition(e){
+       //console.log(e.propertyName);
+       if(e.propertyName!=='transform')return;
+       this.classList.remove("playing");
+   }
+   keys.forEach(key=>{key.addEventListener("transitionend",removeTransition)
+   })
     
